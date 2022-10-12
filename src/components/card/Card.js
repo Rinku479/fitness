d446db62b9d81eba306b3dc0d9ced5e065d56dca
Rinku479 <rinkupaul.47.47@gmail.com>
2022-10-12 19:32:54 +0600

    card.js

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import './Card.css';
 
-const Card = () => {
+const card = (props) => {
+    const { category, age, time, img } = props.workout;
     return (
-        <div>
+        <div className='card'>
+            <div className='card-info'>
+                <img src={img} alt="" />
+                <h3>{category}</h3>
+                <h4>Age Range: {age}</h4>
+                <h4>Duration: {time}</h4>
+            </div>
+            <button className='btn-cart' onClick={() => props.handleTime(time)}>
+                <p>Add to Cart</p>
+            </button>
 
         </div>
     );
 };
 
-export default Card;
+export default card;
