b7b062b984344b4ed09dc7ef2766fc6ec033b13d
Rinku479 <rinkupaul.47.47@gmail.com>
2022-10-12 19:30:54 +0600

    dashboard.js add

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Details from '../details/Details';
+import Card from '../card/Card';
+import "./Dashboard.css"
 
 const Dashboard = () => {
-    return (
-        <div>
+    const [workouts, setWorkout] = useState([]);
 
+    useEffect(() => {
+        fetch('fakeData/workouts.json')
+            .then(res => res.json())
+            .then(data => setWorkout(data))
+    }, [])
+
+    const [duration, setDuration] = useState([]);
+
+    const handleTime = (time) => {
+        const newTime = [...duration, time];
+        setDuration(newTime);
+    }
+
+    return (
+        <div className='dashboard'>
+            <div className='display'>
+                <div className='header'>
+                    <h1>Fitness Hub</h1>
+                    <h4>Select Today's Exercise</h4>
+                </div>
+                <div className='cards'>
+                    {
+                        workouts.map(workout => <Card key={workout.id} workout={workout} handleTime={handleTime}></Card>)
+                    }
+                </div>
+            </div>
+            <Details duration={duration}></Details>
         </div>
     );
 };
